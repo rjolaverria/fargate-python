7a8e779a03f32194beca7c1ebe7bbfaf83ec2bc8
Rjolaverria <rafi13@me.com>
2024-03-02 20:14:28 -0500

    write to queue

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import logging
+import os
+from boto3 import client
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+sqs = client("sqs")
+SQS_QUEUE_URL = os.environ.get("SQS_QUEUE_URL")
 
 def test_function():
     logger.info('Test function called')
     print("Test function called: Updated")
+    sqs.send_message(QueueUrl=SQS_QUEUE_URL, MessageBody="hello world")
 
 
 if __name__ == '__main__':
